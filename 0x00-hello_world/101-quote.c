cf4c376f42d7cbaf3280a39e150af8ee5573f441
mohammed <eldeebmahammed@gmail.com>
2023-02-16 17:19:49 -0800

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,13 @@
 /**
  * main - this function main
  *
+ * description  :print some string 
+ *
  * return : number 1
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
