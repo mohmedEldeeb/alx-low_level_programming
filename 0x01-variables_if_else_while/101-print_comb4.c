74d89a36022d061b4e04015dcff4563ea649efae
mohammed <eldeebmahammed@gmail.com>
2023-02-18 11:23:30 -0800

    d

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int x, y, z;
+
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x < y &&  y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
