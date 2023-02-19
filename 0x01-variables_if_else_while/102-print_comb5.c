54cb77a2bdb20640babb6388ed8249feb1f96e3d
mohammed <eldeebmahammed@gmail.com>
2023-02-19 08:21:12 -0800

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,24 +18,24 @@ int main(void)
 		d2 = 0;
 		for (d2 = '0' ; d2 <= '9' ; d2++)
 		{
-			if (d2 <  d1)
-			{
+			/*if (d2 <  d1)
+			{*/
 				putchar(d1);
 
-				putchar(d2);
+				putchar(d1);
 
 				putchar(' ');
 
 				putchar(d1);
 
-				putchar(d1);
+				putchar(d2);
 
 				if (d2 != '8' || d2 != '9' && d1 != '9')
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
+		/*	}*/
 		}
 	}
 	putchar('\n');
