0202db6aef522c3043294363b777e70bd7cd3a7e
mohammed <eldeebmahammed@gmail.com>
2023-02-19 13:00:55 -0800

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -51,7 +51,7 @@ int main(void)
 				for (i = '0' ; i <= '9'; i++)
 				{
 					
-					if( (x <= z )&& (i + z) > (x y))
+					if( (x <= z )&& (i + z) > (x + y))
 					{
 						putchar(x);
                                                 putchar(y);
