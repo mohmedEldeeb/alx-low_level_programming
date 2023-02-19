a386b74d4c089fbd13d79c011398dc6c8b21e81e
mohammed <eldeebmahammed@gmail.com>
2023-02-19 11:57:14 -0800

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,37 +8,110 @@
  * Return: Always 0 (Success)
  */
 
+
+/*
+ ** let x , y, z, i = 0;
+         * 00 01,to 00 zz
+         * 01 02, 01 zz
+         * 02 03, zz
+         * 66 67, 66 zz
+         * z7 78, z7 zz,
+         * z8 zz
+         * 
+         * while(x > z){
+         *      while(y > z)
+         *              while(z > z)
+         *                      while(i > z)
+         *                              if(x == z , y == z)
+         *                                      putchar(z)
+         *                                      putchar(z)
+         *                                      brack;
+         *
+         *                              if(x<=z && y < i)
+         *                              putchar(x)
+         *                              putchar(y)
+         *                              putchar(' )
+         *                              putchar(z)
+         *                              putchar(i)
+
+         *
+ *
+ */
+
 int main(void)
 {
-
-	int d1 ,d2;
-
-	for (d1 = '0' ; d2 <= '9' ; d1++)
+	int x , y, z, i ;
+	/*x = y = z =i='0';*/
+	for (x = '0' ; x <= '9' ; x++)
 	{
-		d2 = 0;
-		for (d2 = '0' ; d2 <= '9' ; d2++)
+		for (y = '0' ; y <= '9'; y++)
 		{
-			/*if (d2 <  d1)
-			{*/
-				putchar(d1);
-
-				putchar(d1);
-
-				putchar(' ');
-
-				putchar(d1);
-
-				putchar(d2);
-
-				if (d2 != '8' || (d2 != '9' && d1 != '9'))
+			for (z = '0' ; z <= '9'; z++)
+			{
+				for (i = '0' ; i <= '9'; i++)
 				{
+					
+					if ((x == '9') && (y == '9'))
+					{
+						
+						break;
+						break;
+						break;
+						break;
+
+					}
+					if( (x <= z )&& (y < i) )
+					{
+						putchar(x);
+						putchar(y);
+						putchar(' ');
+						putchar(z);
+						putchar(i);
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}
+		}
+			
+	}
+	putchar('9');
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
+
+
+
+/*for (x = '0' ; x <= '9' ; x++)
+{
+	for (y = '0' ; y <='9';y++)
+	{
+		for (z = '0' ; z <='9';z++)
+		{
+			for (i = '0' ; i <='9';i++)
+			{
+				
+				if ((x == 'z') && (y == 'z'))
+				{
+					putchar(x );
+					putchar(y);
+					break;
+				}
+				if((x <= z )&& (y < i) )
+				{
+					putchar(x);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar(i);
 					putchar(',');
 					putchar(' ');
 				}
-		/*	}*/
+
+			}
 		}
 	}
-	putchar('\n');
-
-	return (0);
-}
+		
+}*/
