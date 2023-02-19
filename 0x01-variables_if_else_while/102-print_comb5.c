7f516d14694a2d1b27fa63fae93dd65f461c4c2e
mohammed <eldeebmahammed@gmail.com>
2023-02-19 14:47:55 -0800

    f

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,63 +6,58 @@
  * main - Entry point char
  *
  * Return: Always 0 (Success)
- */
-
-
-/*
- ** let x , y, z, i = 0;
-         * 00 01,to 00 zz
-         * 01 02, 01 zz
-         * 02 03, zz
-         * 66 67, 66 zz
-         * z7 78, z7 zz,
-         * z8 zz
-         * 
-         * while(x > z){
-         *      while(y > z)
-         *              while(z > z)
-         *                      while(i > z)
-         *                              if(x == z , y == z)
-         *                                      putchar(z)
-         *                                      putchar(z)
-         *                                      brack;
-         *
-         *                              if(x<=z && y < i)
-         *                              putchar(x)
-         *                              putchar(y)
-         *                              putchar(' )
-         *                              putchar(z)
-         *                              putchar(i)
-
-         *
+ *
+ * return:  let x , y, z, i = 0
+ * 00 01,to 00 zz
+ * 01 02, 01 zz
+ * 02 03, zz
+ * 66 67, 66 zz
+ * z7 78, z7 zz,
+ * z8 zz
+ *
+ * while(x > z){
+ *      while(y > z)
+ *              while(z > z)
+ *                      while(i > z)
+ *                              if(x == z , y == z)
+ *                                      putchar(z)
+ *                               putchar(z)
+ *                                      brack;
+ *
+ *                              if(x<=z && y < i)
+ *                              putchar(x)
+ *                              putchar(y)
+ *                              putchar(' )
+ *                              putchar(z)
+ *                              putchar(i)
+ *
+ *
  *
  */
 
 int main(void)
 {
-	int x , y, z, i ;
+	int x, y, z, i;
 	/*x = y = z =i='0';*/
-	for (x = '0' ; x <= '9' ; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (y = '0' ; y <= '9'; y++)
+		for (y = '0'; y <= '9'; y++)
 		{
-			for (z = '0' ; z <= '9'; z++)
+			for (z = '0'; z <= '9'; z++)
 			{
-				for (i = '0' ; i <= '9'; i++)
+				for (i = '0'; i <= '9'; i++)
 				{
-					
-					if(((z + i) > (x + y) &&  z >= x) || x < z)
+					if (((z + i) > (x + y) &&  z >= x) || x < z)
 					{
 						putchar(x);
-                                                putchar(y);
-					
+						putchar(y);
 						putchar(' ');
 						putchar(z);
 						putchar(i);
-						if((x == '9') && (y == '8') && (z == '9') && (i =='9'))
-                                        	{
-                                                	break;
-                                        	}
+						if ((x == '9') && (y == '8') && (z == '9') && (i == '9'))
+						{
+							break;
+						}
 						putchar(',');
 						putchar(' ');
 					}
@@ -70,42 +65,8 @@ int main(void)
 				}
 			}
 		}
-			
+
 	}
 	putchar('\n');
 	return (0);
 }
-
-
-
-/*for (x = '0' ; x <= '9' ; x++)
-{
-	for (y = '0' ; y <='9';y++)
-	{
-		for (z = '0' ; z <='9';z++)
-		{
-			for (i = '0' ; i <='9';i++)
-			{
-				
-				if ((x == 'z') && (y == 'z'))
-				{
-					putchar(x );
-					putchar(y);
-					break;
-				}
-				if((x <= z )&& (y < i) )
-				{
-					putchar(x);
-					putchar(y);
-					putchar(' ');
-					putchar(z);
-					putchar(i);
-					putchar(',');
-					putchar(' ');
-				}
-
-			}
-		}
-	}
-		
-}*/
