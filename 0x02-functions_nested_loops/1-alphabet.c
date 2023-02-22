29053bb171ffda1408cd19f89435ea49778cfe79
mohammed <eldeebmahammed@gmail.com>
2023-02-21 17:46:59 -0800

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet: function print char
+ *
+ * return : aonly 0 all time
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
