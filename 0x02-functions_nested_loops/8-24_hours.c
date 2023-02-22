ab1200070e0060a39a4d16e2ec7837ecf62ba9f0
mohammed <eldeebmahammed@gmail.com>
2023-02-22 05:40:25 -0800

    d

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ void jack_bauer(void)
 {
 	int hour, mint;
 
-	for (hour = 0; hour < 24 ;hour++)
+	for (hour = 0; hour < 24 ; hour++)
 	{
-		for (mint = 0; mint < 60;mint++)
+		for (mint = 0; mint < 60; mint++)
 		{
-			_putchar((hour / 10));
-			_putchar((hour % 10));
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
 			_putchar(':');
-			_putchar((mint / 10));
-			_putchar((mint % 10));
+			_putchar((mint / 10) + 48);
+			_putchar((mint % 10) + 48);
 			_putchar('\n');
 		}
 	}
