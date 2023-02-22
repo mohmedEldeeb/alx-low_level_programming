b5fdf8c4638836221e349311bc61dc0280db535c
mohammed <eldeebmahammed@gmail.com>
2023-02-22 05:21:04 -0800

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - return time in day
+ *
+ * Return: time number
+ * 
+ */
+
+
+void jack_bauer(void)
+{
+	int hour, mint;
+
+	for (hour = 0; hour < 24 ;hour++)
+	{
+		for (mint = 0; mint < 60;mint++)
+		{
+			_putchar((hour / 10));
+			_putchar((hour % 10));
+			_putchar(':');
+			_putchar((mint / 10));
+			_putchar((mint % 10));
+			_putchar('\n');
+
