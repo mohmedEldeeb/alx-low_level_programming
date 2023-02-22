4de842abc5c9b68665711eda852d1d53b6743ca5
mohammed <eldeebmahammed@gmail.com>
2023-02-22 09:50:45 -0800

    d

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function to print more number
+ *
+ * @n: strt orom to print
+ */
+
+
+
+
+void print_to_98(int n)
+{
+	int num;
+
+	if (n > 98)
+	{
+		for (num = n; num > 98; --num)
+		{
+			printf("%d, ", num);
+		}
+	} else
+		for (num = n; num < 98; ++num)
+			printf("%d, ",numm);
+	printf("98\n");
+}
