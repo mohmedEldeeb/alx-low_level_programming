2d29a423bd2f76a99c5200f465645cf4313ee4bd
mohammed <eldeebmahammed@gmail.com>
2023-02-22 09:18:35 -0800

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function to add to integer
+ *
+ * @x: pram int
+ *
+ * @z: pram integer
+ *
+ * Return: x + z number
+ */
+
+
+int add(int x, int z)
+{
+	result = x + z;
+
+	return (result);
+}
