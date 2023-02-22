3504805e27c366037f79d9a52951b87eacae9005
mohammed <eldeebmahammed@gmail.com>
2023-02-22 04:34:56 -0800

    d

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_last_digit - this fincton print last digit
  *
  * @n: pram number for function
@@ -14,6 +13,7 @@
 int print_last_digit(int n)
 {
 	int d;
+
 	if (n < 0)
 		d = (n % 10) * -1;
 	else
