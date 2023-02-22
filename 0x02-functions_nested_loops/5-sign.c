95503c92c9fd841e675e41de5d948c4f17e24341
mohammed <eldeebmahammed@gmail.com>
2023-02-22 02:52:20 -0800

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,13 +14,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43 + 0);
+		_putchar(48 + 0);
 		return (1);
-	} else if (n ==0)
+	} else if (n == 0)
 	{
-		_putchar(0 + 48);
+		_putchar(0 + 49);
                 return (0);
-	} else {
+	} else 
+	{
 		putchar(45 + 0);
                 return (-1);
 	
