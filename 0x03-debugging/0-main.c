63e75ff519fcdc2a944bc66f9404ca20e4809deb
mohammed <eldeebmahammed@gmail.com>
2023-02-23 04:39:12 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point function
+ *
+ * Return: 0 number
+ */
+
+
+int main(void)
+{
+	int num;
+	
+	num = 98;
+
+	positive_or_negative(num);
+
+	return (0);
+}
