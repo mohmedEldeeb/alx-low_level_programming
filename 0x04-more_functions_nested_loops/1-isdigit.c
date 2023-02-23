7fc92d8b304e758e2117ee179eccad3b8b6b9f36
mohammed <eldeebmahammed@gmail.com>
2023-02-23 10:46:44 -0800

    debugging

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+
+/**
+ * _isdigit - function check number if from 1 to 9
+ *
+ * Return: 1 if digit or 0 if alse
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+		return (1);
+	else
+		return (0);
+}
