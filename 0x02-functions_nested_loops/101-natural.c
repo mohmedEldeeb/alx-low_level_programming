0e98690e656184b9e9c40f9f7b25cdd9c2cfd0fe
mohammed <eldeebmahammed@gmail.com>
2023-02-22 16:18:14 -0800

    d

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - this is intry point
+ *
+ * Return: always (0) return
+ */
+
+int main(void)
+{
+	int num, result;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 ==0= 0))
+		{
+			result += num;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
