7d606292387f50ac1bc84ce1b0574b73a310d31b
mohammed <eldeebmahammed@gmail.com>
2023-02-23 11:23:25 -0800

    debugging

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+
+/**
+ * mul - multiplies tow number
+ *
+ * @a: first number
+ * @b: scaend number
+ *
+ * Return: number
+ */
+
+int mul(int a, int b)
+{
+	long result;
+
+	result = a * b;
+	return (result);
+}
