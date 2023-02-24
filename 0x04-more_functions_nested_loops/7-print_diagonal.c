34a092bf402b75b0c8466e14191b19a10b646647
mohammed <eldeebmahammed@gmail.com>
2023-02-23 16:43:08 -0800

    x

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal from /
+ *
+ * @n: number of fiagonal
+ */
+
+void print_diagonal(int n)
+{
+	int x, z;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 1; x <= n; ++x)
+		{
+			for (z = 0; z < n; ++z)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
