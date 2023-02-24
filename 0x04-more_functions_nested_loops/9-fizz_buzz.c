75b9b36d876c1597881935b89a8e495a544d4e15
mohammed <eldeebmahammed@gmail.com>
2023-02-23 17:32:23 -0800

    x

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - function to print pizz or fizz interview qusion
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; ++x)
+	{
+		if (x % 3 == 0 && !(x % 5 == 0))
+			printf("Fizz");
+		else if (x % 5 == 0 && !(x % 3 == 0))
+			printf("Buzz");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", x);
+
+		if (x != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
