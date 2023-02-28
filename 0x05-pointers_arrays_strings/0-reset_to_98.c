2e7a90c073312548f2f759f04291411e4f27528d
mohammed <eldeebmahammed@gmail.com>
2023-02-27 17:54:33 -0800

    string

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+
+/**
+ * reset_to_98 - function change value to any int
+ *
+ * @n: prameter to function
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+c longuth staring , array, pounter
