c54ac47d9d8404cd188c21719d5e0f6a6ed9236c
mohammed <eldeebmahammed@gmail.com>
2023-02-27 18:10:09 -0800

    string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+
+/**
+ * swap_int - function to swpe tow number
+ *
+ * @a: number own
+ * @b: tow number
+ *
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
