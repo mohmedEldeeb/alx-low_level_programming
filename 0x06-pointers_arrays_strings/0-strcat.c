a2cb7002edc06ce0de103881bc70cc02945ffa3f
mohammed <eldeebmahammed@gmail.com>
2023-03-02 17:58:30 -0800

    d

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+
+/**
+ * _strcat - function change string
+ *
+ * @dest: pram frist string
+ * @src: pram scand string
+ *
+ * Return: array of char
+ */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	int x = 0;
+	int z = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+	{
+		x++;
+	}
+	for (i = 0; src[1] != '\0'; i++)
+		z++;
+	for (i = 0; i <= z; i++)
+		dest[x + 1] = src[i];
+	return (dest);
+
+}
