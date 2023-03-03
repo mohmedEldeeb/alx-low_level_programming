ed87adb63cde111dc7a9dbb7f12c11efa620dfcf
mohammed <eldeebmahammed@gmail.com>
2023-03-03 15:30:02 -0800

    d

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - function to comper tow string
+ *
+ * @s1: pram own to comper
+ * @s2: pram to to comper
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	do {
+		s1++;
+		s2++;
+	} while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+
+	if (*s1 == *s2)
+		return (0);
+	else
+		(*s1 - *s2);
+
+}
