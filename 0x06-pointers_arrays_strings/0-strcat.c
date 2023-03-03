866cd54af4293f8fa2e94e586058190e75126a70
mohammed <eldeebmahammed@gmail.com>
2023-03-02 18:59:05 -0800

    d

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -20,12 +20,12 @@ char *_strcat(char *dest, char *src)
 	
 	
 	while (dest[x] != '\0')
-		dest++;
+		x++;
 	while (src[z] != 0)
 	{
 		dest = src;
-		dest++;
-		src++;
+		x++;
+		z++;
 	}
 	*dest ='\n';
 	return (s);
