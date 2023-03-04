5e090e4c21878e81d31010190c4bd85a3c15c30f
mohammed <eldeebmahammed@gmail.com>
2023-03-04 08:11:30 -0800

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int index = 0, src_length = 0;
 
 	while (src[index++])
-		src_len++;
+		src_length++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 
-	for (index = src_len; index < n; index++)
+	for (index = src_length; index < n; index++)
 		dest[index] = '\0';
 
-	return (*dest);
+	return (dest);
 }
