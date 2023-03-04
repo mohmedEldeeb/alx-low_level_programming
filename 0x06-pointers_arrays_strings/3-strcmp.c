fbbbbe8fe2dac594b73e91c96a97315834ed5a73
mohammed <eldeebmahammed@gmail.com>
2023-03-03 18:42:37 -0800

    d

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,8 @@ int _strcmp(char *s1, char *s2)
 	do {
 		s1++;
 		s2++;
-	} while ((*s1 && *s2) && (*s1 && *s2));
+	} while ((*s1 && *s2) && (*s1 == *s2));
 
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 - *s2);
+	return (*s1 - *s2);
 
 }
