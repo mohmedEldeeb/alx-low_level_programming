2204d7c480f59f68484d95454ce930512a505a9b
mohammed <eldeebmahammed@gmail.com>
2023-03-03 18:51:27 -0800

    d

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_number - function to return Prints for  integer.
+ *
+ * @n: The integer for printed.
+ *
+ * Return: no return
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar((x % 10) + '0');
+}
