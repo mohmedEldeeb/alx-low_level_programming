6d596b85210102964831e5085fd4d710ece5feb2
mohammed <eldeebmahammed@gmail.com>
2023-03-06 18:03:39 -0800

    d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+
+/**
+ * strchr - dunction string char
+ *
+ * @s: pram pounter to char
+ * @c: char to pounter
+ *
+ * Return: for to this poinr=ter
+ */
+
+
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
