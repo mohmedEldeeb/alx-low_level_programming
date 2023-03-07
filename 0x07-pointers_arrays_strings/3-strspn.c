9c480ad2ca28fa3ab3ccf6554200246bd1d17f87
mohammed <eldeebmahammed@gmail.com>
2023-03-06 18:26:06 -0800

    d

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z , x, y;
+
+	z = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
+
+}
