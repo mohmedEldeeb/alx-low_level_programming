e44a2681072a91636b13b235773f8d3151807082
mohammed <eldeebmahammed@gmail.com>
2023-03-06 17:26:22 -0800

    d

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+
+/**
+ * _memset - function to memset number
+ *
+ * @s: number to make thing i don'know
+ * @b: ofcorse char we need
+ * @n: bosteve number
+ *
+ * Return char new
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+
+}
