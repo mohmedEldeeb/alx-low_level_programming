9b9e655a590ec4447206ebb3922d7ee4c97bed1b
mohammed <eldeebmahammed@gmail.com>
2023-03-08 16:25:33 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,14 @@
  * @s: is string
  */
 
-void _puts_recursion(char s)
+void _puts_recursion(char *s)
 {
 	int i = 0;
 	
-	if (s[i] != '\0')
-	{
-		i++;
-		_puts_recursion(s[i]);
-	}
+	if (s[i] == '\0')
+		_putchar('\n');
+	i++;
+	_puts_recursion(s[i]);
+	
         _putchar(s[i]);
 }
