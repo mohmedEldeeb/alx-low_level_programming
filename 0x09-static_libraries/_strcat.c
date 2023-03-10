34e28ddc7e15c29d81c065ed967e037b57c6c963
mohammed <eldeebmahammed@gmail.com>
2023-03-10 15:57:40 -0800

    static

---
+++ 0x09-static_libraries/_strcat.c
@@ -0,0 +1,32 @@
+/**
+ * _strcat - function change string
+ *
+ * @dest: pram frist string
+ * @src: pram scand string
+ *
+ * Return: array of char
+ */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+
+	int x, z;
+
+	x = 0;
+	z = 0;
+
+
+	while (dest[x] != '\0')
+		x++;
+	while (src[z] != 0)
+	{
+		dest[x] = src[z];
+		x++;
+		z++;
+	}
+	dest[x] = '\0';
+
+	return (dest);
+}
