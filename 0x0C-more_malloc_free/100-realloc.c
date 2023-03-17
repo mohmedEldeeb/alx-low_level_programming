0296045390897750c264e63e56887438a859ec19
mohammed <eldeebmahammed@gmail.com>
2023-03-16 19:08:58 -0700

    mallec

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,8 +16,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *m;
-	char *ptr_copy, *f;
+	void *memory;
+	char *ptr_copy, *filler;
 	unsigned int index;
 
 	if (new_size == old_size)
@@ -25,12 +25,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		m = malloc(new_size);
+		memory = malloc(new_size);
 
-		if (m == NULL)
+		if (memory == NULL)
 			return (NULL);
 
-		return (m);
+		return (memory);
 	}
 
 	if (new_size == 0 && ptr != NULL)
@@ -40,19 +40,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	ptr_copy = ptr;
-	m = malloc(sizeof(*ptr_copy) * new_size);
+	memory = malloc(sizeof(*ptr_copy) * new_size);
 
-	if (m == NULL)
+	if (memory == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	filler = mem;
+	filler = memory;
 
 	for (index = 0; index < old_size && index < new_size; index++)
-		f[index] = *ptr_copy++;
+		filler[index] = *ptr_copy++;
 
 	free(ptr);
-	return (m);\
+	return (memory);
 }
