29c767a75a9e78a318c846eed2e683eca3642dea
mohammed <eldeebmahammed@gmail.com>
2023-03-16 17:33:32 -0700

    mallec

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function to return pounter or 98 if null
+ *
+ * @b: lenth in momery
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (i == NULL)
+		exit(98);
+
+	return (x);
+
+}
