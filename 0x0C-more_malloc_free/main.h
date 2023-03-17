ce6fc7790186e0ff956d1b3ad3831ad0799412d9
mohammed <eldeebmahammed@gmail.com>
2023-03-16 17:36:12 -0700

    mallec

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void _print(char *str, int l);
+
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+
+int check_for_digits(char **av);
+
+void init(char *str, int l);
+
+#include <stdio.h>
+
+#endif/*MAIN_H*/
+
