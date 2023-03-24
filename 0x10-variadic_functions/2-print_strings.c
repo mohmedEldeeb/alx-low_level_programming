8f70dc08aa88ffab529dde3fea7d02511fc30be5
mohammed <eldeebmahammed@gmail.com>
2023-03-23 19:41:15 -0700

    d

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,28 +8,29 @@
  * @n: number of prams
  * @...: sprit prams
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list strings;
+	va_list s;
 	char *str;
-	unsigned int index;
+	unsigned int i;
 
-	va_start(strings, n);
+	va_start(s, n);
 
-	for (index = 0; index < n; index++)
+	for (i = 0; i < n; i++)
 	{
-		str = va_arg(strings, char *);
+		str = va_arg(s, char *);
 
 		if (str == NULL)
 			printf("(nil)");
 		else
 			printf("%s", str);
 
-		if (index != (n - 1) && separator != NULL)
+		if (i != (n - 1) && s != NULL)
 			printf("%s", separator);
 	}
 
 	printf("\n");
 
-	va_end(strings);
+	va_end(s);
 }
