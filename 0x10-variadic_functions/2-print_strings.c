98ec3b5deb15be23f9564c821868c5ad631c4a1f
mohammed <eldeebmahammed@gmail.com>
2023-03-23 19:15:29 -0700

    variasdic-function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function to Prints strings
+ *
+ * @separator: string to add sperattor
+ * @n: number of prams
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	unsigned int i;
+	va_list ap;
+
+	va_start(x, n);
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (x == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
