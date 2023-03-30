66eac221bfdcdcba0feffc7a4644ad602a3bd36c
mohammed <eldeebmahammed@gmail.com>
2023-03-30 12:11:42 -0700

    list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,15 +11,15 @@
 
 int get_strlen(const char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
-        return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
-
 
 /**
  * add_node_end - add new node to the end at linked list
