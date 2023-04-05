ccf63df36e26be65fce014f0117289e469407b2a
mohammed <eldeebmahammed@gmail.com>
2023-04-04 20:01:51 -0700

    d

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,7 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
-	
+
 	if (!head)
 		return (NULL);
 
@@ -31,6 +31,6 @@ listint_t *find_listint_loop(listint_t *head)
 		}
 	}
 
-	
+
 	return (NULL);
 }
