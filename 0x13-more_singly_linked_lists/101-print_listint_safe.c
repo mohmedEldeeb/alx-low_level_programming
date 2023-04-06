08c9f9d66d671b0af6109e64d403f0ea9a106cac
mohammed <eldeebmahammed@gmail.com>
2023-04-05 19:37:51 -0700

    x

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list
+ *
+ * @head: hade list
+ *
+ * Return: number
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+    size_t num = 0;
+    long int x;
+
+    while (head)
+    {
+        x = head - head->next;
+        num++;
+        printf("[%p] %d\n", (void *)head, head->n);
+        if (x > 0)
+            head = head->next;
+        else
+        {
+            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+            break;
+        }
+    }
+
+    return (num);
+}
