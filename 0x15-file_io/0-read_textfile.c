5cd62958cba65f4c6b5e69ebe733af42fc0a1a6c
mohammed <eldeebmahammed@gmail.com>
2023-04-10 18:07:49 -0700

    first point

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads taxt from file
+ *
+ * @filename: filename
+ * @letters: numbers
+ *
+ * Return: count letter or 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, result;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	printf("\nfd %d\n",fd);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	result = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (result);
+}
