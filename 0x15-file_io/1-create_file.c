49d5e4bca24e8b6b6dd2adcd06461cb2fcbf6878
mohammed <eldeebmahammed@gmail.com>
2023-04-10 18:23:04 -0700

    d

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fwrite;
+	int n;
+	int *fd;
 
 	if (!filename)
 		return (-1);
@@ -25,12 +25,12 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (n = 0; text_content[n]; n++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	fwrite = write(fd, text_content, n);
 
-	if (rwr == -1)
+	if (fwrite == -1)
 		return (-1);
 
 	close(fd);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,7 +10,9 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+
 int create_file(const char *filename, char *text_content);
+
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
