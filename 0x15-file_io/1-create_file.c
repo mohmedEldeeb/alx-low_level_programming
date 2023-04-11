ceaa6d973e3148906f93552b43c4ae418350edcf
mohammed <eldeebmahammed@gmail.com>
2023-04-10 19:06:18 -0700

    f

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * create_file - creates a file and add content
  *
- * Return: 1 if it success. -1 if it fails.
+ * @filename: filename
+ * @text_content: writed to file.
+ *
+ * Return: 1 or -1
  */
 
 int create_file(const char *filename, char *text_content)
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ *
+ * @filename: filename
+ * @text_content: add to file
+ *
+ * Return: 1 or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, n, fwriet;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (n = 0; text_content[nletters]; nletters++)
+			;
+
+		fwriet = write(fd, text_content, n);
+
+		if (fwriet == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
