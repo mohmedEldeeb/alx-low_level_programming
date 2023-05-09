5fae14cec926db51625167001b4ecd1f48eddc18
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-05-09 04:39:36 +0300

    fole-3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -64,7 +64,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_version(header->e_ident);
 	print_osabi(header->e_ident);
 	print_abi(header->e_ident);
-	print_type(header->e_type, header->e_ident);
+	print_type(header->e_ident, header->e_type);
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
